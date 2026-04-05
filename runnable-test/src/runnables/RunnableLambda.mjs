ff0096a7f6acd6496a203385ddb58cd5133ed60c
wujingde <wujingde@zhuanzhuan.com>
2026-04-05 22:20:13 +0800

    feat: 合并

---
+++ runnable-test/src/runnables/RunnableLambda.mjs
@@ -0,0 +1,18 @@
+import dotenv from "dotenv"
+dotenv.config({ override: true })
+import { RunnableLambda, RunnableSequence } from "@langchain/core/runnables"
+
+const addOne = RunnableLambda.from(input => {
+  console.log(`输入: ${input}`)
+  return input + 1
+})
+
+const multiplyTwo = RunnableLambda.from(input => {
+  console.log(`输入: ${input}`)
+  return input * 2
+})
+
+const chain = RunnableSequence.from([addOne, multiplyTwo, addOne])
+
+const result = await chain.invoke(5)
+console.log(result)
